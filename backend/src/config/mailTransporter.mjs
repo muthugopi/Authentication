1268f68351ef7ae476f2e95871d02998b1cc9383
Muthu Gopi <massg754@gmail.com>
2026-01-15 16:46:06 +0530

    resolved

--- backend/src/config/mailTransporter.mjs
+++ backend/src/config/mailTransporter.mjs
@@ -2,12 +2,18 @@ import nodemailer from "nodemailer";
 
 const transporter = nodemailer.createTransport({
   service: "gmail",
-  auth: { user: process.env.EMAIL, pass: process.env.EMAIL_PASS },
-  logger: true, 
+  auth: { 
+    user: process.env.EMAIL, 
+    pass: process.env.EMAIL_PASS 
+  },
+  logger: true,
   debug: true,   
 });
+
 
 transporter.verify((err, success) => {
   if (err) console.log("SMTP error:", err);
   else console.log("SMTP ready:", success);
 });
+
+export default transporter;
