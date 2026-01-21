2f1f4028ced107d117cd1d12b8278fe4b2a29bc2
Muthu Gopi <massg754@gmail.com>
2026-01-21 18:27:56 +0530

    The End

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,31 +1,23 @@
 import { BrowserRouter, Routes, Route } from "react-router-dom";
-import Landing from "./pages/Landing";
-import MakeMeAdmin from "./pages/MakeMeAdmin";
-import Login from "./pages/Login";
-import Register from "./pages/Register";
-import AdminPanel from "./pages/AdminPanel";
-import PublicMessage from "./pages/PublicMessage";
-import Navbar from "./components/Navbar";
-import NotFound from "./components/NotFound";
-import ToastContainer from './components/ToastContainer'
-import Profile from "./pages/Profile";
+// import Landing from "./pages/Landing";
+// import MakeMeAdmin from "./pages/MakeMeAdmin";
+// import Login from "./pages/Login";
+// import Register from "./pages/Register";
+// import AdminPanel from "./pages/AdminPanel";
+// import PublicMessage from "./pages/PublicMessage";
+// import Navbar from "./components/Navbar";
+// import NotFound from "./components/NotFound";
+// import ToastContainer from './components/ToastContainer'
+// import Profile from "./pages/Profile";
+import Closed from "./pages/Closed";
+
 import './index.css'
 
 function App() {
   return (
     <BrowserRouter>
-      <Navbar />  
-      <ToastContainer />
       <Routes>
-        <Route path="/" element={<Landing />} />
-        <Route path="/verify-admin" element={<MakeMeAdmin />} />
-        <Route path="/register" element={<Register />} />
-        <Route path="/login" element={<Login />} />
-        <Route path="/admin" element={<AdminPanel />} />
-        <Route path='/message' element={<PublicMessage />} />
-        <Route path="/profile" element={<Profile />} />
-
-        <Route path="*" element={<NotFound />} />
+        <Route path="*" element={<Closed />} />
       </Routes>
     </BrowserRouter>
   );
